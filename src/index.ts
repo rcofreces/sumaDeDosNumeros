dc098c77e21446ad2821eaa7b583b7bd83eddd5b
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-16 14:30:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let numero1: number = Number(prompt("Ingrese el número 1"));
+let numero2: number = Number(prompt("Ingrese el número 2"));
+let resultado: number = numero1 + numero2;
+
+console.log("El primer número es:", numero1);
+console.log("El segundo número es:", numero2);
+console.log("La suma de los números es:", resultado);
